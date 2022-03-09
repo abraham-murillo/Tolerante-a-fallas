79a5d47de3f402e9811c63305f097a01371511af
Abraham Baltazar Murillo Sandoval <abraham@Abrahams-MacBook-Air.local>
2022-03-08 18:41:32 -0600

    Espero que jale :c

--- hamming/main.cpp
+++ hamming/main.cpp
@@ -1,6 +1,7 @@
 // http://archive.dimacs.rutgers.edu/drei/1997/classroom/lessons/hamming.html
+// https://users.cs.fiu.edu/~downeyt/cop3402/hamming.html
 
-// Computación tolerante a fallas, D02, Equipo 02
+// Computación tolerante a fallas, D01, Equipo 02
 #include <bitset>
 #include <cmath>
 #include <iostream>
@@ -42,9 +43,46 @@ struct Hamming {
     return hammingCode.size() - k;
   }
 
-  string calculateHamming(const string& input) {
-    if (isValid(input)) {
-      // cout << "Valido\n";
+  void toLowerCase(string& s) {
+    for (char& c : s)
+      c = tolower(c);
+  }
+
+  string toBinary(string word) {
+    string binary;
+    for (char c : word) {
+      bitset<5> bs(c - 'a');
+      // cout << c << " " << bs.to_string() << '\n';
+      binary += bs.to_string();
+    }
+    return binary;
+  }
+
+  string toAlpha(const string& binary) {
+    string word;
+    for (int position = 0, pw = 1; position < binary.size();) {
+      bitset<5> bs;
+      for (int cnt = 0; cnt < 5 && position < binary.size(); position++) {
+        if (position == pw - 1) {
+          pw *= 2;
+          continue;
+        }
+        // cout << position + 1 << " ";
+        bs[4 - cnt] = binary[position] - '0';
+        cnt++;
+      }
+      // cout << '\n';
+      cout << char('a' + bs.to_ulong()) << '\n';
+      word += char('a' + bs.to_ulong());
+    }
+    return word;
+  }
+
+  string calculateHamming(string& word) {
+    toLowerCase(word);
+
+    if (isValid(word, "az")) {
+      string input = toBinary(word);
 
       string output(outputSize(input), '-');
       auto it = input.begin();
@@ -81,36 +119,39 @@ struct Hamming {
 
       return output;
     } else {
-      // cout << "Invalido\n";
+      cout << "La cadena contiene caracteres que no corresponden al alfabeto.\n";
       return "";
     }
   }
 
-  int findErrorsHamming(string hammingCode) {
+  void findErrorsHamming(string hammingCode) {
     if (isValid(hammingCode) && hammingCode.size() > 4) {
-      cout << "Es una string válida\n";
-
       string correctHammingCode = findCorrectHammingCode(hammingCode);
+
+      cout << "Entrada recibida: " << hammingCode << '\n';
+      cout << "Hamming correcto: " << correctHammingCode << '\n';
+
       if (correctHammingCode == hammingCode) {
         cout << "No hay errores\n";
       } else {
-        cout << "Hamming correcto : " << correctHammingCode << '\n';
         if (correctHammingCode.size() == hammingCode.size()) {
-          cout << "Errores en las posiciones : ";
+          cout << "Errores en las posiciones: ";
           for (int index = 0; index != hammingCode.size(); ++index) {
             if (correctHammingCode[index] != hammingCode[index])
               cout << " " << index + 1;
           }
+          cout << '\n';
         } else {
           cout << "Hamming inválido\n";
         }
       }
+
+      // Busca qué palabra de español es la más parecida
+      cout << toAlpha(correctHammingCode) << '\n';
+
     } else {
       cout << "Entrada inválida\n";
-      return -1;
     }
-
-    return 0;
   }
 
   string findCorrectHammingCode(const string& hammingCode) {
@@ -142,64 +183,79 @@ struct Hamming {
     }
   }
 
-  void toLowerCase(string& s) {
-    for (char& c : s)
-      c = tolower(c);
-  }
+  void selfTest() {
+    string str;
+    for (int i = 0; i < 5; i++)
+      str += char('a' + rand() % 26);
 
-  string toBinary(string word) {
-    string binary;
-    // reverse(word.begin(), word.end());
-    for (char c : word) {
-      bitset<5> bs(c - 'a');
-      // cout << c << " " << bs.to_string() << '\n';
-      binary += bs.to_string();
+    cout << str << '\n';
+    auto hamming = calculateHamming(str);
+    cout << hamming << '\n';
+
+    int wrong = 0;
+    int total = 0;
+    for (int i = 0; i < hamming.size(); i++) {
+      auto hammingCopy = hamming;
+      hammingCopy[i] = hammingCopy[i] ^ 1;
+      auto correct = findCorrectHammingCode(hammingCopy);
+
+      if (correct != hamming) {
+        cout << toAlpha(correct) << " != " << str << '\n';
+        wrong++;
+      }
+
+      total++;
     }
-    return binary;
-  }
 
-  string toAlpha(const string& binary) {
-    cout << "Binario: " << binary << '\n';
-    string word;
-    for (int i = 0; i < binary.size(); i += 5) {
-      bitset<5> bs(binary.substr(i, 5));
-      cout << binary.substr(i, 5) << " " << bs.to_ulong() << '\n';
-      word += char('a' + bs.to_ulong());
-    }
-    return word;
-  }
-
-  string solve(string& word) {
-    toLowerCase(word);
-    if (isValid(word, "az")) {
-      string binary = toBinary(word);
-      string hamming = findCorrectHammingCode(binary);
-
-      cout << toAlpha(binary) << '\n';
-      cout << toAlpha(hamming) << '\n';
-
-      return hamming;
-    } else {
-      return "La cadena contiene caracteres que no corresponden al alfabeto.";
-    }
+    cout << 100 * double(wrong) / double(total) << "%\n";
   }
 };
 
 int main() {
   Hamming hamming;
 
-  while (1) {
-    cout << "Ingresa una palabra del español o el caracter '?' para salir \n";
-    string word;
-    cin >> word;
+  // hamming.selfTest();
 
-    if (word == "?") {
-      cout << "Fin\n";
-      break;
+  string op;
+
+  do {
+    cout << "Seleccione una opción\n";
+    cout << "1. Conocer el código de hamming de una palabra\n";
+    cout << "2. Validar un código de hamming\n";
+    cout << "3. Salir \n";
+
+    cin >> op;
+    cout << op << '\n';
+
+    switch (op[0]) {
+      case '1': {
+        cout << "Ingresa una palabra del español\n";
+        string word;
+        cin >> word;
+        cout << hamming.calculateHamming(word) << '\n';
+        break;
+      }
+
+      case '2': {
+        cout << "Ingresa un código de hamming\n";
+        string binary;
+        cin >> binary;
+        hamming.findErrorsHamming(binary);
+        break;
+      }
+
+      case '3':
+        cout << "Fin\n";
+        break;
+
+      default:
+        break;
     }
 
-    cout << hamming.solve(word) << '\n';
-  }
+    cout << '\n';
+  } while (op[0] != '3');
 
   return 0;
 }
+
+// 010111111001001000011001101110
