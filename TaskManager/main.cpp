41e192e2c129c5239101fdace1710042efa58ec8
abraham-murillo <abraham140600@gmail.com>
2022-05-04 01:07:27 -0500

    Ya jala al 100 :D

--- TaskManager/main.cpp
+++ TaskManager/main.cpp
@@ -3,6 +3,7 @@
 // - Guzmán Mendoza Jesús Uriel
 // - Murillo Sandoval Abraham Baltazar
 // - Valencia Curiel Armando Saul
+// - Villalpando Armenta Marco Antonio
 
 #include <algorithm>
 #include <array>
@@ -11,6 +12,7 @@
 #include <conio.h>
 #include <cstdio>
 #include <fstream>
+#include <iomanip>
 #include <iostream>
 #include <map>
 #include <queue>
@@ -21,14 +23,9 @@
 #include <vector>
 #include <windows.h>
 
-using namespace std::this_thread; // sleep_for, sleep_until
-using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 using namespace std;
 
-// Tiempo que espera en pantalla
-const auto WAIT_ON_SCREEN = 1s;
 const int BAR_HEIGHT = 5;
-const int BAR_WIDTH = 3;
 const int BLACK = 0;
 const int WHITE = 15;
 int columns, rows;
@@ -40,12 +37,12 @@ struct Task {
   int x, y;
 };
 
-void paint(int x, int y, int color) {
+void paint(int x, int y, int color, char ch = ' ') {
   // Hacer que pinte (x, y) con cierto color
-  COORD coord = {x, rows - y};
+  COORD coord = {x, y};
   SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
   SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
-  cout << " ";
+  cout << ch;
 }
 
 struct Numbers {
@@ -83,11 +80,12 @@ struct Numbers {
       reverse(digits.begin(), digits.end());
     }
 
-    for (int row = 0; row < 5; row++)
+    for (int row = 0; row < 5; row++) {
+      int k = 0;
       for (int i = 0; i < digits.size(); i++)
         for (char ch : number[digits[i]][row])
-          if (ch == '#')
-            paint(x + i, y + row, color);
+          paint(x + i + k++, y + row, color, ch);
+    }
   }
 } numbers;
 
@@ -116,11 +114,11 @@ int main() {
 
   int maxEnd = 0;
   vector<Task> tasks;
-  int lastY = columns - 2 * BAR_HEIGHT;
+  int lastY = BAR_HEIGHT + 3;
   for (int id = 0; id < n; id++) {
     Task task;
     int duration;
-    cout << "Escriba cuando inicia y cuánto dura el proceso:\n";
+    cout << "Escriba cuando inicia y cu�nto dura el proceso:\n";
     cin >> task.start >> duration;
     task.end = task.start + duration - 1;
     task.id = id;
@@ -131,21 +129,26 @@ int main() {
     tasks.emplace_back(task);
 
     maxEnd = max(maxEnd, task.end);
-    lastY -= BAR_HEIGHT;
+    lastY += BAR_HEIGHT;
   }
 
   // Escalar las barras
   // Tengo que hacer maxEnd pedazos en columns
-  int barWidth = max(1, columns / maxEnd);
-
+  int barWidth = max(1, columns / (maxEnd + 1));
+  // cout << barWidth << endl;
   for (Task& task : tasks) {
-    task.start = task.start * columns / maxEnd;
-    task.end = task.end * columns / maxEnd;
-
-    task.x = task.start * barWidth;
+    task.x = (task.start * barWidth);
+    // cout << task.id << " " << task.x << " " << task.y << endl;
   }
 
   clearScreen();
+
+  paint(0, lastY + 2, WHITE);
+  cout << "Datos de los procesos\n";
+  cout << "Proceso  | Inicio   | Duracion\n";
+  for (Task& task : tasks) {
+    cout << setw(7) << "P" + to_string(task.id) << setw(10) << task.start << setw(10) << task.end - task.start + 1 << "\n";
+  }
 
   sort(tasks.begin(), tasks.end(), [&](const Task& a, const Task& b) {
     if (a.start != b.start)
@@ -153,8 +156,22 @@ int main() {
     return a.end < b.end;
   });
 
-  int clockX = 0;
-  int clockY = rows - 1;
+  vector<int> byEndTime(n);
+  for (int i = 0; i < n; i++)
+    byEndTime[i] = i;
+  sort(byEndTime.begin(), byEndTime.end(), [&](int i, int j) {
+    if (tasks[i].end != tasks[j].end)
+      return tasks[i].end < tasks[j].end;
+    return tasks[i].start < tasks[j].start;
+  });
+
+  for (Task& task : tasks) {
+    paint(task.x - 4, task.y + 2, WHITE);
+    cout << task.id;
+  }
+
+  int clockX = 8;
+  int clockY = 1;
   set<pair<int, int>> inExecution; // {end, task position}
   int lastPos = 0;
   for (int globalTime = 0; globalTime <= maxEnd + 1; globalTime++) {
@@ -171,12 +188,13 @@ int main() {
 
     // Pintarlo de negro para "borrarlo"
     numbers.print(clockX, clockY, BLACK, globalTime - 1);
+
     // Pintarlo de nuevo pero en blanco
     numbers.print(clockX, clockY, WHITE, globalTime);
 
     vector<int> colors = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16};
-    for (auto&& [end, pos] : inExecution) {
-      Task& task = tasks[pos];
+    for (auto it : inExecution) {
+      Task& task = tasks[it.second];
       // Pintar con gotoxy()
       // Se supone que cada task tiene un (x, y), pintar el cuadrado siguiente
       for (int i = 0; i < barWidth; i++)
@@ -186,7 +204,16 @@ int main() {
       task.x += barWidth;
     }
 
-    sleep_for(WAIT_ON_SCREEN);
+    paint(0, lastY + 2 + tasks.size() + 4, WHITE);
+    cout << "Orden de ejecucion\n";
+    cout << "Proceso  |  Ejecucion\n";
+    for (int i : byEndTime) {
+      Task& task = tasks[i];
+      if (globalTime >= task.start) {
+        cout << setw(7) << "P" + to_string(task.id) << setw(10) << min(globalTime, task.end) << "\n";
+      }
+    }
+    Sleep(1000);
   }
 
   return 0;
